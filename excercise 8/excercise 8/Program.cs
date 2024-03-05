dd4e5332ab868d8ed0bcb64121d1b30b9468dd23
Soggy Water <145466434+EWalker623@users.noreply.github.com>
2024-03-05 09:15:18 -0500

    completed exercise 8

---
+++ excercise 8/excercise 8/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+internal class program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("enter maximum number ");
+        int number = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++)
+        {
+
+            //for loop body
+            if (i % 2 == 0)
+            {
+                Console.WriteLine();
+            }
+
+        Console.WriteLine(i);
+            {
+
+                //print asterisk row 
+                Console.WriteLine("how many rows of astrik? ");
+                int astNum = Int32.Parse(Console.ReadLine());
+
+                for (int a = 0; a < number; a++)
+                {
+                    Console.Write("*");
+                }
+
+                //Console.WriteLine(maxNum)
+                
+                
+                
+                
+                
+                
+            
+
+
+
+
+           
+            
+
+            
+
+
+
+
